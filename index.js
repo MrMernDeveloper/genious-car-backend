5374a43641cb14d9f17c2f28c40dc7c4ecfec228
MrMernDeveloper <rakibkst16@gmail.com>
2022-11-02 22:10:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle wares
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h32cfqq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('genius car server is running')
+})
+app.listen(port, () => {
+    console.log(`Genius Car server running on ${port} `)
+})
